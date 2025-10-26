1062982e6cc66137218c5aa533ca99d64ccb145c
anuragreddy1 <anuragreddy996@gmail.com>
2025-10-26 17:22:49 +0000

    message

---
+++ src/fcfs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+struct Process()
+{
+    int pid;//process id
+    int arrival_time;
+    int burst_time;
+    int turnaround_time;//completion time - arrival time
+    int completion_time;
+    int waiting_time;
+    int priority;
+    int remaining_time;
+}
+int main()
+{
+
+}
